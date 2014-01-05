d46a4cc997cdffc265e517856d228e22dc00bb82
Richard Lasjunies <rlasjunies@gmail.com>
2014-01-05 20:51:02 +0100

    EndTheTuto

--- CanvasFilters/app.ts
+++ CanvasFilters/app.ts
@@ -1,28 +1,73 @@
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+/// <reference path="Scripts/typings/jquery/jquery.d.ts" />
+/// <reference path="Canvas.ts" />
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
+function grayscale() {
+    var c: mtg.util.canvas.Canvas = new mtg.util.canvas.Canvas( <HTMLImageElement> document.getElementById( 'orig' ) );
+    var imgDest: HTMLImageElement = <HTMLImageElement>document.getElementById( 'dest' );
+    c.grayscale();    
+    imgDest.src = c.canvas.toDataURL();
 }
 
-window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-};
+function brightness() {
+    var c: mtg.util.canvas.Canvas = new mtg.util.canvas.Canvas( <HTMLImageElement> document.getElementById( 'orig' ) );
+    var imgDest: HTMLImageElement = <HTMLImageElement>document.getElementById( 'dest' );
+    c.brightness();
+    imgDest.src = c.canvas.toDataURL();
+}
+
+function threshold() {
+    var c: mtg.util.canvas.Canvas = new mtg.util.canvas.Canvas( <HTMLImageElement> document.getElementById( 'orig' ) );
+    var imgDest: HTMLImageElement = <HTMLImageElement>document.getElementById( 'dest' );
+    c.threshold();
+    imgDest.src = c.canvas.toDataURL();
+}
+
+function sharpen() {
+    var c: mtg.util.canvas.Canvas = new mtg.util.canvas.Canvas( <HTMLImageElement> document.getElementById( 'orig' ) );
+    var imgDest: HTMLImageElement = <HTMLImageElement>document.getElementById( 'dest' );
+    c.convolute( [ 0, -1,  0,
+                  -1,  5, -1,
+                   0, -1,  0], 1);
+    imgDest.src = c.canvas.toDataURL();
+}
+
+function myblur() {
+    var c: mtg.util.canvas.Canvas = new mtg.util.canvas.Canvas( <HTMLImageElement> document.getElementById( 'orig' ) );
+    var imgDest: HTMLImageElement = <HTMLImageElement>document.getElementById( 'dest' );
+    c.convolute( [1/9,1/9,1/9,1/9,1/9,1/9,1/9,1/9,1/9], 1 );
+    imgDest.src = c.canvas.toDataURL();
+}
+
+function sobel() {
+    var gray: mtg.util.canvas.Canvas = new mtg.util.canvas.Canvas( <HTMLImageElement> document.getElementById( 'orig' ) );
+    var imgDest: HTMLImageElement = <HTMLImageElement>document.getElementById( 'dest' );
+    gray.grayscale();
+
+    imgDest.src = gray.canvas.toDataURL();
+
+                                                  
+    var vertical = jQuery.extend( true, {}, gray );
+    var horizontal = jQuery.extend( true, {}, gray );
+
+    // Note that ImageData values are clamped between 0 and 255, so we need
+    // to use a Float32Array for the gradient values because they
+    // range between -255 and 255.
+    var src1 = vertical.convoluteFloat32( 
+        [-1, 0, 1,
+            -2, 0, 2,
+            -1, 0, 1], 1 );
+    imgDest.src = vertical.canvas.toDataURL();
+
+    var src2 = horizontal.convoluteFloat32( 
+        [-1, -2, -1,
+            0, 0, 0,
+            1, 2, 1], 1 );
+
+    gray.mergeSobel( src1, src2 );
+    gray.canvas.width = 700;
+    gray.canvas.height = 337;
+
+    imgDest.src = gray.canvas.toDataURL();
+
+
+}
